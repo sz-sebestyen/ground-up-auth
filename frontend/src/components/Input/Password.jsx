7fa237308fd2044596e2f471470beabfdf650f64
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-21 22:43:16 +0200

    Validate password

--- frontend/src/components/Input/Password.jsx
+++ frontend/src/components/Input/Password.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Input from "./Input";
 
-function Password(props) {
+function Password({ ...rest }) {
   return (
     <Input
       label="Password:"
       required
       name="password"
       type="password"
-      {...props}
+      {...rest}
+      pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.-_]).{8,}"
     />
   );
 }
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -2,25 +2,45 @@ import React, { useState } from "react";
 import { Username, Email, Password } from "../components/Input";
 
 function Registration() {
-  const [dto, setDto] = useState({ username: "", email: "", password: "" });
+  const [dto, setDto] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
   const handleInputChange = async ({ target: { name, value } }) => {
     setDto((dto) => ({ ...dto, [name]: value }));
   };
 
-  const register = async () => {
-    console.log(dto);
+  const register = async ({ target }) => {
+    if (dto.password !== dto.password2) {
+      target.parentElement
+        .querySelector('input[name="password2"]')
+        .setCustomValidity("Reapeat password!");
+    } else {
+      target.parentElement
+        .querySelector('input[name="password2"]')
+        .setCustomValidity("");
+    }
+    const isValid = target.parentElement.querySelector("form").reportValidity();
+    console.log(dto, isValid);
   };
 
   return (
     <div>
       <div>
-        <Username value={dto.username} onChange={handleInputChange} />
-        <Email value={dto.email} onChange={handleInputChange} />
-        <Password value={dto.password} onChange={handleInputChange} />
-        <div>
-          <button onClick={register}>Register</button>
-        </div>
+        <form>
+          <Username value={dto.username} onChange={handleInputChange} />
+          <Email value={dto.email} onChange={handleInputChange} />
+          <Password value={dto.password} onChange={handleInputChange} />
+          <Password
+            value={dto.password2}
+            onChange={handleInputChange}
+            name="password2"
+          />
+        </form>
+        <button onClick={register}>Register</button>
       </div>
     </div>
   );
